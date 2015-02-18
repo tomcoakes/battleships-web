1d1f6a4c8bdb0ef692354cd104f1f16e545c725f
Tom Coakes <tomcoakes@gmail.com>
2015-02-18 12:23:43 +0000

    Add submit button to form

--- features/step_definitions/starting_a_game_steps.rb
+++ features/step_definitions/starting_a_game_steps.rb
@@ -3,15 +3,15 @@ Given(/^I am on the homepage$/) do
 end
 
 When(/^I click "(.*?)"$/) do |arg1|
-  click_link(arg1)
+  click_on(arg1)
 end
 
 When(/^I enter my name as "(.*?)"$/) do |name|
-  fill_in('name', :with => name)
+  fill_in('name', :with => "name")
 end
 
 When(/^I press submit$/) do
-  pending # express the regexp above with the code you wish you had
+  click_button('submit')
 end
 
 Then(/^I should see "(.*?)"$/) do |arg1|
