33254f1ff93399643947e86596f67e94c4b4aaf6
Tom Coakes <tomcoakes@gmail.com>
2015-02-19 18:46:20 +0000

    Game can add two players

--- battleships.rb
+++ battleships.rb
@@ -3,19 +3,19 @@ require_relative './lib/board'
 require_relative './lib/cell'
 require_relative './lib/ship'
 require_relative './lib/water'
+require_relative './lib/game'
+require_relative './lib/player'
 
 class Battleships < Sinatra::Base
   set :views, Proc.new { File.join(root, "views") }
+  enable :sessions
 
+  game = Game.new
   board = Board.new(size: 2, content: Cell)
   ship = Ship.new(1)
-  p board.matrix
 
   get '/' do
-    board.grid.values.each do |cell|
-      cell.content = Water.new
-    end
-
+    board.grid.values.each { |cell| cell.content = Water.new }
     random_coord = board.grid.keys.sample
     board.place(ship, random_coord)
     erb :index
@@ -26,10 +26,13 @@ class Battleships < Sinatra::Base
   end
 
   post '/new' do
-    @name = params[:name]
-      if @name.empty?
+    name = params[:name]
+      if name.empty?
         erb :new
       else
+        @player = Player.new(name)
+        game.add_player(@player)
+        p game
         @grid = board.grid
         erb :board
       end
